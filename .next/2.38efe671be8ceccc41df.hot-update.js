8f75b8e931e3f6b49d0451e6428636d062b7706c
Morton358 <vovankovalchuk358@gmail.com>
2018-03-17 23:40:57 +0100

    remove .next

--- .next/2.38efe671be8ceccc41df.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(2,[])
-//# sourceMappingURL=2.38efe671be8ceccc41df.hot-update.js.map
--- .next/2.8a97b13b27ae203585ca.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(2,[])
-//# sourceMappingURL=2.8a97b13b27ae203585ca.hot-update.js.map
